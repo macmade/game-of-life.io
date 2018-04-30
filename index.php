f49d18e2d52f62767abd205225db56636b19c147
macmade <macmade@xs-labs.com>
2018-04-30 21:26:19 +0200

    Base HTML

--- index.php
+++ index.php
@@ -2,8 +2,59 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
     <head>
+        
+        <!--
+        
+        ##################################################
+        #                                                #
+        #       Blood Sweat & Code (& Rock'N'Roll)       #
+        #      Thanx for looking at the source code      #
+        #                                                #
+        #                 XS-Labs © 2013                 #
+        #                                                #
+        ##################################################
+        
+        -->
+        
         <meta http-equiv="content-type" content="text/html; charset=utf-8" />
         <title>Game Of Life</title>
+        
+        <link rel="stylesheet" href="/css/styles.php" type="text/css" media="all" />
+        <meta name="author" content="XS-Labs" />
+        <meta name="description" content="XS-Labs is the home of the XEOS Operating System and related projects." />
+        <meta name="keywords" content="XS-Labs, XEOS, XSFoundation, Codeine, ClangKit, FileSystem, XEOS Software Licence, OpenSource, Operating System, Development, Cocoa, iOS, Objective-C, Assembly, x86, C++, Code, Software, Mac, IDE, Macmade, Jean-David, Gadina, Switzerland" />
+        <meta name="rating" content="General" />
+        <meta name="robots" content="all" />
+        <!--[if IE]><link rel="shortcut icon" href="/favicon.ico"><![endif]-->
+        <link rel="icon" href="/favicon.png">
+        <link rel="apple-touch-icon-precomposed" href="/favicon-apple-touch.png">
+        <script type="text/javascript">
+            // <![CDATA[
+            
+            if( ( ( window.devicePixelRatio === undefined ) ? 1 : window.devicePixelRatio ) > 1 )
+            {
+                document.cookie = 'X_XSLABS_CLIENT_IS_RETINA=1;path=/';
+            }
+            
+            // ]]>
+        </script>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-51035898-8"></script>
+        <script type="text/javascript">
+            // <![CDATA[
+            
+            window.dataLayer = window.dataLayer || [];
+            
+            function gtag()
+            {
+                dataLayer.push( arguments );
+            }
+            
+            gtag( 'js', new Date() );
+            gtag( 'config', 'UA-51035898-8' );
+            
+            // ]]>
+        </script>
     </head>
     <body>
         hello, world
